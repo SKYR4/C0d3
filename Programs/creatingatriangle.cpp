a7cc95fc725b207d711075ee27a61857cbffd1be
Akshat Agrawal <49070489+akshatagrawal9874@users.noreply.github.com>
2020-10-02 11:51:45 +0530

    Update the pattern of right angled triangle program

--- Programs/creatingatriangle.cpp
+++ Programs/creatingatriangle.cpp
@@ -4,10 +4,41 @@ using namespace std;
 
 int main()
 {
-cout << "     /|" << endl;
-cout << "    / |" << endl;
-cout << "   /  |" << endl;
-cout << "  /   |" << endl;
-cout << " /____|" << endl;
+int i, j, rows;
+
+    // Input rows from user
+    cout<<"Enter number of rows : "<<endl;
+    cin>>rows;
+
+    //Creating triangle Logic:
+    for(i=1; i<=rows; i++)
+    {
+        for(j=i; j<rows; j++)
+        {
+            cout<<" ";
+        }
+
+        for(j=1; j<=i; j++)
+        {
+            if(i==rows || j==1 || j==i)
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+
+        cout<<endl;
+    }
+
     return 0;
 }
+//Output :
+Enter number of rows: 5
+    *
+   **
+  * *
+ *  *
+*****
