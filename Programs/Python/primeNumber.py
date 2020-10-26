4eedb9dfae1d07934b6f098f5856b23ede982d88
Ayush Mehrotra <ayushmehrotra1234@gmail.com>
2020-10-27 01:39:27 +0530

    Update primeNumber.py
    added user input and better algo for larger numbers

--- Programs/Python/primeNumber.py
+++ Programs/Python/primeNumber.py
@@ -1,11 +1,10 @@
-num = 407
-
-
+import cmath
+num = int(input('Enter a number you want to check'))
 
 if num > 1:
 
    for i in range(2,num):
-       if (num % i) == 0:
+       if (cmath.sqrt(num) % i) == 0:
            print(num,"is not a prime number")
            print(i,"times",num//i,"is",num)
            break
